a30c918ad7e7a44a609da2be27914d68dec207d1
MonzKA <111332166+MonzKA@users.noreply.github.com>
2022-09-27 23:16:11 +0530

    Update main.py
    Signed-off-by: MonzKA <111332166+MonzKA@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 Bot = Client(
-    "Telegraph Uploader Bot",
+    "Tgmediauploaderbot",
     bot_token=os.environ.get("BOT_TOKEN"),
     api_id=int(os.environ.get("API_ID")),
     api_hash=os.environ.get("API_HASH")
